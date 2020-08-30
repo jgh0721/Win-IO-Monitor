465435864c5b67465742f0b18860d2ffe8366af2
gyuho.jeong <gyuho.jeong@gytni.com>
2020-08-30 21:58:10 +0900

    cntl: WinIOMonitor Controller project added

---
+++ src/cntl/WinIOMonitorControl.cpp
@@ -0,0 +1,12 @@
+﻿#include "stdafx.h"
+#include "WinIOMonitorControl.h"
+
+#if defined(_MSC_VER)
+#   pragma execution_character_set( "utf-8" )
+#endif
+
+WinIOMonitorControl::WinIOMonitorControl( QWidget* parent )
+    : QMainWindow( parent )
+{
+    ui.setupUi( this );
+}
---
+++ src/cntl/WinIOMonitorControl.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include "ui_WinIOMonitorControl.h"
+
+#if defined(_MSC_VER)
+#   pragma execution_character_set( "utf-8" )
+#endif
+
+class WinIOMonitorControl : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    WinIOMonitorControl(QWidget *parent = Q_NULLPTR);
+
+private:
+    Ui::WinIOMonitorControlClass ui;
+};
