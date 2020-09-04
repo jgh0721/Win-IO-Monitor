b8c5c82f22f15c9ad28c8eed51ef87b5559c6754
gyuho.jeong <gyuho.jeong@gytni.com>
2020-09-04 15:26:25 +0900

    cntl: QtitanDataGrid files into projects added cmn: QtitanDataGrid files on gitignore added sys: IRP_MJ_QUERY_INFORMATION empty handler added

--- src/sys/callbacks/fltQueryInformation.cpp
+++ src/sys/callbacks/fltQueryInformation.cpp
@@ -1,5 +1,7 @@
 ﻿#include "fltQueryInformation.hpp"
 
+#include "WinIOMonitor_W32API.hpp"
+
 #if defined(_MSC_VER)
 #   pragma execution_character_set( "utf-8" )
 #endif
@@ -10,7 +12,23 @@ FLT_PREOP_CALLBACK_STATUS FLTAPI WinIOPreQueryInformation( PFLT_CALLBACK_DATA Da
 
     __try
     {
-        
+        auto FileInformationClass = (nsW32API::FILE_INFORMATION_CLASS)Data->Iopb->Parameters.QueryFileInformation.FileInformationClass;
+
+        switch( FileInformationClass )
+        {
+            case nsW32API::FileAllInformation: {} break;
+            case nsW32API::FileAttributeTagInformation: {} break;
+            case nsW32API::FileBasicInformation: {} break;
+            case nsW32API::FileCompressionInformation: {} break;
+            case nsW32API::FileEaInformation: {} break;
+            case nsW32API::FileInternalInformation: {} break;
+            case nsW32API::FileMoveClusterInformation: {} break;
+            case nsW32API::FileNameInformation: {} break;
+            case nsW32API::FileNetworkOpenInformation: {} break;
+            case nsW32API::FilePositionInformation: {} break;
+            case nsW32API::FileStandardInformation: {} break;
+            case nsW32API::FileStreamInformation: {} break;
+        }
     }
     __finally
     {
@@ -22,5 +40,31 @@ FLT_PREOP_CALLBACK_STATUS FLTAPI WinIOPreQueryInformation( PFLT_CALLBACK_DATA Da
 
 FLT_POSTOP_CALLBACK_STATUS FLTAPI WinIOPostQueryInformation( PFLT_CALLBACK_DATA Data, PCFLT_RELATED_OBJECTS FltObjects, PVOID CompletionContext, FLT_POST_OPERATION_FLAGS Flags )
 {
+
+    auto FileInformationClass = ( nsW32API::FILE_INFORMATION_CLASS )Data->Iopb->Parameters.QueryFileInformation.FileInformationClass;
+
+    __try
+    {
+        switch( FileInformationClass )
+        {
+            case nsW32API::FileAllInformation: {} break;
+            case nsW32API::FileAttributeTagInformation: {} break;
+            case nsW32API::FileBasicInformation: {} break;
+            case nsW32API::FileCompressionInformation: {} break;
+            case nsW32API::FileEaInformation: {} break;
+            case nsW32API::FileInternalInformation: {} break;
+            case nsW32API::FileMoveClusterInformation: {} break;
+            case nsW32API::FileNameInformation: {} break;
+            case nsW32API::FileNetworkOpenInformation: {} break;
+            case nsW32API::FilePositionInformation: {} break;
+            case nsW32API::FileStandardInformation: {} break;
+            case nsW32API::FileStreamInformation: {} break;
+        }
+    }
+    __finally
+    {
+        
+    }
+
     return FLT_POSTOP_FINISHED_PROCESSING;
 }
