4f171e0312d9ed85a2fa85c14951463479276aaf
gyuho.jeong <gyuho.jeong@gytni.com>
2020-09-21 14:20:49 +0900

    sys-isolation: CacheManager Callbacks - Read-ahead implementation

--- src/sys-isolation/callbacks/cacheManagerCallbacks.cpp
+++ src/sys-isolation/callbacks/cacheManagerCallbacks.cpp
@@ -1,11 +1,17 @@
 ﻿#include "cacheManagerCallbacks.hpp"
 
+#include "privateFCBMgr.hpp"
+#include "privateFCBMgr_Defs.hpp"
+
 #if defined(_MSC_VER)
 #   pragma execution_character_set( "utf-8" )
 #endif
 
 BOOLEAN CcAcquireForLazyWrite( PVOID Context, BOOLEAN Wait )
 {
+    auto Fcb = ( FCB* )Context;
+    UNREFERENCED_PARAMETER( Wait );
+
     return FALSE;
 }
 
@@ -15,9 +21,27 @@ void CcReleaseFromLazyWrite( PVOID Context )
 
 BOOLEAN CcAcquireForReadAhead( PVOID Context, BOOLEAN Wait )
 {
-    return FALSE;
+    auto Fcb = ( FCB* )Context;
+    UNREFERENCED_PARAMETER( Wait );
+
+    FltAcquireResourceShared( &Fcb->MainResource );
+
+    KdPrint( ( "[WinIOSol] %s Open=%d Clean=%d Ref=%d Name=%ws\n"
+               , __FUNCTION__
+               , Fcb->OpnCount, Fcb->ClnCount, Fcb->RefCount
+               , Fcb->FileFullPath.Buffer ) );
+
+    return TRUE;
 }
 
 void CcReleaseFromReadAhead( PVOID Context )
 {
+    auto Fcb = ( FCB* )Context;
+
+    FltReleaseResource( &Fcb->MainResource );
+
+    KdPrint( ( "[WinIOSol] %s Open=%d Clean=%d Ref=%d Name=%ws\n"
+               , __FUNCTION__
+               , Fcb->OpnCount, Fcb->ClnCount, Fcb->RefCount
+               , Fcb->FileFullPath.Buffer ) );
 }
--- src/sys-isolation/callbacks/cacheManagerCallbacks.hpp
+++ src/sys-isolation/callbacks/cacheManagerCallbacks.hpp
@@ -7,6 +7,12 @@
 #   pragma execution_character_set( "utf-8" )
 #endif
 
+/*!
+    캐시관리자에 의해 시스템 스레드에서 호출된다
+
+    해당 콜백들은 캐시관리자에서 스트림을 읽고/쓰기 전에 락 획득/해제를 위해 호출하는 콜백이다 
+*/
+
 EXTERN_C_BEGIN
 
 BOOLEAN CcAcquireForLazyWrite( IN PVOID Context, IN BOOLEAN Wait );
