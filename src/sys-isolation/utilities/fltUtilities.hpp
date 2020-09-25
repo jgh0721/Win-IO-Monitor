aa12a5b81d8127cf49b9c3f60e764a26732ae19e
gyuho.jeong <gyuho.jeong@gytni.com>
2020-09-25 10:06:40 +0900

    sys-isolation: filter driver utilities added( empty files )

---
+++ src/sys-isolation/utilities/fltUtilities.hpp
@@ -0,0 +1,10 @@
+﻿#ifndef HDR_DRIVER_UTILITIES
+#define HDR_DRIVER_UTILITIES
+
+#include "fltBase.hpp"
+
+#if defined(_MSC_VER)
+#   pragma execution_character_set( "utf-8" )
+#endif
+
+#endif // HDR_DRIVER_UTILITIES
