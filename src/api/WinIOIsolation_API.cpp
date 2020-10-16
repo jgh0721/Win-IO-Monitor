1912f5ec558b3023dce61df6685ba1c54ea87f1c
gyuho.jeong <gyuho.jeong@gytni.com>
2020-10-16 11:03:30 +0900

    sys-isolation: WinIOIsolation API prepare

---
+++ src/api/WinIOIsolation_API.cpp
@@ -0,0 +1,145 @@
+﻿#include "WinIOIsolation_API.hpp"
+
+#include "WinIOIsolation_IOCTL.hpp"
+#include "WinIOIsolation_Names.hpp"
+
+#include <process.h>
+
+#pragma comment( lib, "fltlib" )
+
+#if defined(_MSC_VER)
+#   pragma execution_character_set( "utf-8" )
+#endif
+
+///////////////////////////////////////////////////////////////////////////////
+
+struct
+{
+    HANDLE                          hPort = INVALID_HANDLE_VALUE;
+    HANDLE                          hDevice = INVALID_HANDLE_VALUE;
+
+} GlobalContext;
+
+///////////////////////////////////////////////////////////////////////////////
+
+HRESULT ConnectTo()
+{
+    HRESULT hRet = S_OK;
+
+    do
+    {
+        if( GlobalContext.hPort != INVALID_HANDLE_VALUE )
+        {
+            break;
+        }
+
+        hRet = FilterConnectCommunicationPort( PORT_NAME, 0,
+                                               NULL, 0, NULL,
+                                               &GlobalContext.hPort );
+
+        if( FAILED( hRet ) )
+            break;
+
+    } while( false );
+
+    return hRet;
+}
+
+HRESULT Disconnect()
+{
+    if( GlobalContext.hPort != INVALID_HANDLE_VALUE )
+        CloseHandle( GlobalContext.hPort );
+    GlobalContext.hPort = INVALID_HANDLE_VALUE;
+
+    return S_OK;
+}
+
+DWORD SetDriverStatus( BOOLEAN IsRunning )
+{
+    DWORD dwRet = ERROR_SUCCESS;
+    DWORD dwRetLen = 0;
+    HANDLE hDevice = INVALID_HANDLE_VALUE;
+
+    do
+    {
+        hDevice = CreateFileW( L"\\\\." DEVICE_NAME,
+                               GENERIC_READ | GENERIC_WRITE,
+                               FILE_SHARE_READ | FILE_SHARE_WRITE,
+                               NULL,
+                               OPEN_EXISTING,
+                               FILE_ATTRIBUTE_NORMAL,
+                               NULL );
+
+        if( hDevice == INVALID_HANDLE_VALUE )
+        {
+            dwRet = GetLastError();
+            break;
+        }
+
+        BOOL bSuccess = DeviceIoControl( hDevice,
+                                    IOCTL_SET_DRIVER_STATUS,
+                                    &IsRunning,
+                                    sizeof(BOOLEAN),
+                                    NULL,
+                                    0,
+                                    ( LPDWORD )&dwRetLen,
+                                    NULL );
+
+        if( !bSuccess )
+        {
+            dwRet = GetLastError();
+            break;
+        }
+        
+    } while( false );
+
+    if( hDevice != INVALID_HANDLE_VALUE )
+        CloseHandle( hDevice );
+
+    return dwRet;
+}
+
+DWORD GetDriverStatus( BOOLEAN* IsRunning )
+{
+    DWORD dwRet = ERROR_SUCCESS;
+    DWORD dwRetLen = 0;
+    HANDLE hDevice = INVALID_HANDLE_VALUE;
+
+    do
+    {
+        hDevice = CreateFileW( L"\\\\." DEVICE_NAME,
+                               GENERIC_READ | GENERIC_WRITE,
+                               FILE_SHARE_READ | FILE_SHARE_WRITE,
+                               NULL,
+                               OPEN_EXISTING,
+                               FILE_ATTRIBUTE_NORMAL,
+                               NULL );
+
+        if( hDevice == INVALID_HANDLE_VALUE )
+        {
+            dwRet = GetLastError();
+            break;
+        }
+
+        BOOL bSuccess = DeviceIoControl( hDevice,
+                                         IOCTL_GET_DRIVER_STATUS,
+                                         NULL,
+                                         0,
+                                         IsRunning,
+                                         sizeof(BOOLEAN),
+                                         ( LPDWORD )&dwRetLen,
+                                         NULL );
+
+        if( !bSuccess )
+        {
+            dwRet = GetLastError();
+            break;
+        }
+
+    } while( false );
+
+    if( hDevice != INVALID_HANDLE_VALUE )
+        CloseHandle( hDevice );
+
+    return dwRet;
+}
---
+++ src/cmn/WinIOIsolation_API.hpp
@@ -0,0 +1,51 @@
+﻿#ifndef HDR_WINIOISOLATION_API
+#define HDR_WINIOISOLATION_API
+
+#if defined(USE_ON_KERNEL)
+#include "fltBase.hpp"
+#else
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <windows.h>
+#include <winternl.h>
+#include <fltUser.h>
+#include <fltUserStructures.h>
+#endif
+
+#include "WinIOIsolation_Event.hpp"
+
+#if defined(_MSC_VER)
+#   pragma execution_character_set( "utf-8" )
+#endif
+
+///////////////////////////////////////////////////////////////////////////////
+
+/*!
+    1. ConnectTo
+    2. SetDriverConfig
+    3. SetDriverStatus
+    
+    Set ProcessId(All), Rename, all Encrypted File
+*/
+
+HRESULT ConnectTo();
+HRESULT Disconnect();
+
+DWORD SetDriverConfig( __in DRIVER_CONFIG* DriverConfig );
+DWORD GetDriverConfig( __in DRIVER_CONFIG* DriverConfig );
+
+// 
+DWORD SetDriverStatus( __in BOOLEAN IsRunning );
+DWORD GetDriverStatus( __in BOOLEAN* IsRunning );
+
+DWORD AddGlobalFilterMask( __in const wchar_t* wszFilterMask, __in bool isInclude );
+DWORD DelGlobalFilterMask( __in const wchar_t* wszFilterMask, __in bool isInclude );
+DWORD ResetGlobalFilter();
+
+
+DWORD CollectNotifyEventItmes( __inout PVOID Buffer, __in ULONG BufferSize, __out ULONG* WrittenItemCount );
+
+#endif // HDR_WINIOISOLATION_API
