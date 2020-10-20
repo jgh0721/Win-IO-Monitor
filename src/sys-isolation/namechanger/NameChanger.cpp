2ab871cf12cf50982a4f76cf840d552202a94452
gyuho.jeong <gyuho.jeong@gytni.com>
2020-10-20 10:48:56 +0900

    sys-isolation: NameChanger feature prepare for type3 metadata

---
+++ src/sys-isolation/namechanger/NameChanger.cpp
@@ -0,0 +1,44 @@
+﻿#include "NameChanger.hpp"
+
+#include "fltCmnLibs.hpp"
+
+#if defined(_MSC_VER)
+#   pragma execution_character_set( "utf-8" )
+#endif
+
+BOOLEAN CheckNameChanger( IRP_CONTEXT* IrpContext, TyGenericBuffer<WCHAR>* SrcFileFullPath )
+{
+    BOOLEAN IsConcerned = FALSE;
+
+    do
+    {
+        if( IrpContext == NULLPTR || SrcFileFullPath == NULLPTR || SrcFileFullPath->Buffer == NULLPTR || SrcFileFullPath->BufferSize == 0 )
+            break;
+
+        /*!
+            Name Change Condition ( AND Condition )
+
+            1. File Name : ABCDEF.XYZ.EXE
+            2. Must be exist metadata info in file( Hidden File )
+        */
+
+        if( IrpContext->Fcb == NULLPTR )
+            break;
+
+        if( !FlagOn( IrpContext->Fcb->Flags, FCB_STATE_METADATA_ASSOC ) )
+            break;
+
+        auto Suffix = nsUtils::EndsWithW( SrcFileFullPath->Buffer, L".exe" );
+        if( Suffix == NULLPTR )
+            break;
+
+        auto Midfix = nsUtils::ForwardFindW( SrcFileFullPath->Buffer, L'.' );
+        if( Midfix == Suffix )
+            break;
+
+        IsConcerned = TRUE;
+
+    } while( false );
+
+    return IsConcerned;
+}
---
+++ src/sys-isolation/namechanger/NameChanger.hpp
@@ -0,0 +1,21 @@
+﻿#ifndef HDR_ISOLATION_NAME_CHANGER
+#define HDR_ISOLATION_NAME_CHANGER
+
+#include "fltBase.hpp"
+
+#include "irpContext_Defs.hpp"
+#include "NameChanger_Defs.hpp"
+
+#if defined(_MSC_VER)
+#   pragma execution_character_set( "utf-8" )
+#endif
+
+/**
+ * @brief 
+ * @param IrpContext 
+ * @param SrcFileFullPath 
+ * @return TRUE : NameChanger 의 대상
+*/
+BOOLEAN CheckNameChanger( __in IRP_CONTEXT* IrpContext, __in TyGenericBuffer<WCHAR>* SrcFileFullPath );
+
+#endif // HDR_ISOLATION_NAME_CHANGER
